6e1c6bee1861304d2ef81f8a33b98e07ac14159b
Rianmachado123 <rianmchazm@gmail.com>
2026-02-07 16:35:05 -0300

    feat: adicionando contador de palavra/paragrafo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs= require('fs');
+const { text } = require('stream/consumers');
 
 
 
@@ -12,8 +13,38 @@ fs.readFile(caminho, 'utf-8', (err, data) => {
         console.error('Erro ao ler o arquivo:', err);
         return;
     }
-    console.log('Conteúdo do arquivo:', data);
+    quebreDeParagrafo(data);
+    // verificarPalavraDuplicadas(data);
 }   );
 
 console.log(caminho);
 
+
+// criar um array para armazenar as palavras do texto
+//contar as concorrência 
+//montar um objeto com resultado 
+
+// {
+//     'web': 3,
+//     'java': 2,
+//     'python': 1
+// }
+function verificarPalavraDuplicadas(texto){
+    const listaPAlavras=texto.split(' ');
+    const resultado={};
+    listaPAlavras.forEach(palavra => {
+        resultado[palavra] = resultado[palavra] ? resultado[palavra] + 1 : 1;
+
+})
+console.log(resultado);
+};
+
+
+function quebreDeParagrafo(texto){
+    const paragrafos= texto.toLowerCase().split('\n');
+    const resultado= paragrafos.map(paragrafo => {
+        return verificarPalavraDuplicadas(paragrafo);
+    });
+    console.log(paragrafos);
+    
+}
